e7c55041a3e180ee15b88229e895d2b1e3158763
Vinicius Prado <vinicavagna@gmail.com>
2021-11-01 15:18:50 -0300

    :sparkles: added function to add spotify songs and playlists(only top 100 musics)

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -7,8 +7,10 @@ import {
   StreamType,
 } from "@discordjs/voice";
 import { VoiceChannel } from "discord.js";
+import { getData } from "spotify-url-info";
 import ytdl from "ytdl-core";
 import ytpl from "ytpl";
+import ytsr from "ytsr";
 import { ServerQueueProps } from "../interfaces/Queue";
 
 const playSong = async (
@@ -50,7 +52,7 @@ const addPlaylistToQueue = async (interaction: any, input: string) => {
 
   const serverQueue = interaction.client.queue.get(guildId);
 
-  const playlistData = await ytpl(input);
+  const playlistData = await ytpl(input, { limit: 500 });
 
   const playlistVideos = playlistData.items;
 
@@ -110,16 +112,13 @@ const createQueue = async (interaction: any, guildId: string) => {
   }
 };
 
-const addVideoToQueue = async (interaction: any, input: string) => {
+const addVideoToQueue = async (
+  interaction: any,
+  song: { title: string; url: string },
+) => {
   const guildId = interaction.guild.id;
 
   const serverQueue = interaction.client.queue.get(guildId);
-
-  const songInfo = await ytdl.getInfo(input);
-  const song = {
-    title: songInfo.videoDetails.title,
-    url: songInfo.videoDetails.video_url,
-  };
 
   if (!serverQueue) {
     const queueConstruct = await createQueue(interaction, guildId);
@@ -140,6 +139,33 @@ const addVideoToQueue = async (interaction: any, input: string) => {
   return song.title;
 };
 
+const addSpotifySong = async (interaction: any, name: any) => {
+  const resultSearch = (await ytsr(name, { limit: 1 })) as any;
+  const video = resultSearch.items[0].url;
+  await addVideoToQueue(interaction, video);
+};
+
+const addSpotifyPlaylist = async (interaction: any, data: any) => {
+  const names = data.tracks.items.map((item: any) => item.track.name);
+
+  await Promise.allSettled(
+    names.map(async (name: string) => {
+      const result = await ytsr.getFilters(name);
+
+      const videoFilter = result.get("Type")?.get("Video") as any;
+
+      const resultSearch = (await ytsr(videoFilter.url, { limit: 1 })) as any;
+
+      const song = {
+        url: resultSearch.items[0].url,
+        title: resultSearch.items[0].title,
+      };
+      await addVideoToQueue(interaction, song);
+    }),
+  );
+  return names.length;
+};
+
 const play = {
   data: new SlashCommandBuilder()
     .setName("play")
@@ -147,7 +173,8 @@ const play = {
     .addStringOption((string) =>
       string
         .setName("input")
-        .setDescription("Play a given song name/URL in the voice channel"),
+        .setDescription("Play a given song name/URL in the voice channel")
+        .setRequired(true),
     ),
   async execute(interaction: any) {
     const voiceChannel = interaction.member.voice.channel as VoiceChannel;
@@ -170,16 +197,61 @@ const play = {
 
     const input = interaction.options.getString("input") as string;
 
-    if (input.includes("youtu")) {
-      if (ytdl.validateURL(input)) {
-        const song = await addVideoToQueue(interaction, input);
+    if (ytdl.validateURL(input)) {
+      await interaction.reply("Working on it");
 
-        return interaction.reply(`Queued **${song}**`);
-      } else {
-        const items = await addPlaylistToQueue(interaction, input);
+      const songInfo = await ytdl.getInfo(input);
+      const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+      };
 
-        return interaction.reply(`Queued **${items} songs**`);
+      const songName = await addVideoToQueue(interaction, song);
+
+      return interaction.editReply(`Queued **${songName}**`);
+    }
+
+    if (input.includes("youtube.com/playlist")) {
+      await interaction.reply("Working on it");
+
+      const items = await addPlaylistToQueue(interaction, input);
+
+      return interaction.editReply(`Queued **${items} songs**`);
+    }
+
+    if (input.includes("spotify.com")) {
+      const data = await getData(input);
+
+      switch (data.type) {
+        case "playlist":
+          await interaction.reply("Working on it");
+
+          const items = await addSpotifyPlaylist(interaction, data);
+
+          return interaction.editReply(`Queued **${items} songs**`);
+        case "track":
+          await interaction.reply("Working on it");
+
+          await addSpotifySong(interaction, data.name);
+
+          return interaction.editReply(`Queued **${data.name}**`);
+        default:
+          break;
       }
+    }
+
+    if (!input.includes("http")) {
+      await interaction.reply("Working on it");
+
+      const resultSearch = (await ytsr(input, { limit: 1 })) as any;
+      const song = {
+        url: resultSearch.items[0].url,
+        title: resultSearch.items[0].title,
+      };
+
+      const songName = await addVideoToQueue(interaction, song);
+
+      return interaction.editReply(`Queued **${songName}**`);
     }
   },
 };
